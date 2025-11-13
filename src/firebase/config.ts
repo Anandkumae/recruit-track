2a1a0e66f8960bb888a1b01373b6c2a6cda19c7f
Anand <ramashankarsingh841@gmail.com>
2025-11-13 08:33:06 +0000

    this is the required data-# Firebase Configuration NEXT_PUBLIC_FIREBASE_

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-7488920972-4ef9c",
-  "appId": "1:340682426505:web:4fadac2d08617994135e58",
-  "storageBucket": "studio-7488920972-4ef9c.firebasestorage.app",
   "apiKey": "AIzaSyAeaZ48PWG0xi3SHhEvHUDBpiIGTRmd16c",
   "authDomain": "studio-7488920972-4ef9c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "340682426505"
+  "projectId": "studio-7488920972-4ef9c",
+  "storageBucket": "studio-7488920972-4ef9c.firebasestorage.app",
+  "messagingSenderId": "340682426505",
+  "appId": "1:340682426505:web:4fadac2d08617994135e58",
+  "measurementId": ""
 };
